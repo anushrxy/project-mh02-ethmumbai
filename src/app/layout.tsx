448e1f5536ac8c238363db1cdb7b7a1d38f43908
SirSimon162 <anmolpattnayak2016@gmail.com>
2024-04-14 22:09:01 +0530

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./Providers";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.png"/>
+      </Head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
